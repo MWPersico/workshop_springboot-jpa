08a53d5b7421a1081bf8ae0b270b4c544f29054e
Marcos Willian <willian.marcos7@hotmail.com>
2024-03-19 16:48:14 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/marcprojects/springdemo/controllers/UserController.java
+++ src/main/java/com/marcprojects/springdemo/controllers/UserController.java
@@ -1,18 +1,30 @@
 package com.marcprojects.springdemo.controllers;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcprojects.springdemo.entities.User;
+import com.marcprojects.springdemo.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping(value = {"/", ""})
-	public ResponseEntity<User> findAll() {
-		return ResponseEntity.ok(new User(1, "Marcos", "willian.marcos7@hotmail.com", "1234", "4321"));
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Integer id){
+		return userRepository.findById(id).get();
 	}
 }
---
+++ src/main/java/com/marcprojects/springdemo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.marcprojects.springdemo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcprojects.springdemo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{}
