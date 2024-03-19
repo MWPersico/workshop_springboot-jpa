05f1b6000d951c56c2191fed3ca24e56a3fbd6c5
Marcos Willian <willian.marcos7@hotmail.com>
2024-03-19 16:21:27 -0300

    H2 database, JPA, test

--- src/main/java/com/marcprojects/springdemo/entities/User.java
+++ src/main/java/com/marcprojects/springdemo/entities/User.java
@@ -2,10 +2,19 @@ package com.marcprojects.springdemo.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
